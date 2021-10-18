b445fc9f1136f2bee4ab6da0fdbc381f79c350bb
ABHI12800 <abhiasawale510@gmail.com>
2021-10-18 10:13:03 +0530

    completed amtToWords.py can print number till Quattuordecillion

--- Day1-5-10-21/amtToWords.py
+++ Day1-5-10-21/amtToWords.py
@@ -1,35 +1,35 @@
 # Convert Number to Words
+import math
+def toHundreds(number):
+    # print(number)
+    numberStrings = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen"
+    ,"Nineteen"]
+    tensString = ["","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"]
+    word = ""
+    length = len(number)
+    twoDigits = int(number[-2::])
+    if(len(number)==3):
+        word = word + numberStrings[int(number[length-3])]
+        if(int(number[length-3])!=0):
+            word += "Hundred "
+    if(twoDigits <= 19):
+        word += numberStrings[twoDigits]
+    else:
+        word = word + tensString[int(number[length-2])] + numberStrings[int(number[length-1])]
+    return word
+
 
 def convertToWord(number):
+    n = len(number)
     if(int(number)==0):
         return "Zero"
     WordString = ""
-    numberStrings = ["","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen"
-    ,"Nineteen"]
-    tensString = ["","","Twenty","Thirty","Fourty","Fifty","Sixty","Seventy","Eighty","Ninety"]
-    tenMultipleString = ["","","Hundred ","Thousand ","Thousand"]
-    tensMultipleCount = 0
-
-    if(int(number)<=19):
-        return (numberStrings[int(number)])
-    else:
-        LastTwoDigits = int(number[-2:])
-        startingDigit = 0
-        if(LastTwoDigits<=19):
-            startingDigit=2
-            WordString = numberStrings[LastTwoDigits]
-            tensMultipleCount+=2
-        number = number[::-1]
-        for i in range(startingDigit,len(number)):
-            digit = int(number[i])
-            if(i>=2):
-                if(digit!=0):
-                    WordString = tenMultipleString[tensMultipleCount] + WordString
-            if(i%2!=0 and tensMultipleCount==1):
-                WordString = tensString[digit]+WordString
-            else:
-                WordString = numberStrings[digit] +  WordString
-            tensMultipleCount+=1
+    tenMultipleString = ["","Thousand ","Million ","Billion ","Trillion ","Quadrillion ","Quintillion ","Sextillion ","Septillion ","Octillion ","Nonillion ","Decillion ","Undecillion ","Duodecillion ","Tredecillion ","Quattuordecillion "]
+    for i in range(math.ceil(len(number)/3)):
+        returnedWord = toHundreds(number[-3*(1+i):n-(3*i):]) 
+        if(returnedWord!=""):
+            WordString = tenMultipleString[i] + WordString 
+        WordString = returnedWord + WordString
     return WordString
 
 
